1b3d7e1e7891813cafb7b130c236db576dff7c1f
liu <normidar7@gmail.com>
2023-01-28 13:12:40 +0900

    u

--- lib/dpro_editor.dart
+++ lib/dpro_editor.dart
@@ -1,7 +1,6 @@
-library dpro_editor;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// run this to reset your file:  dart run build_runner build
+// or use flutter:               flutter packages pub run build_runner build
+// remenber to format this file, you can use: dart format
+// publish your package hint: dart pub publish --dry-run
+// if you want to update your packages on power: dart pub upgrade --major-versions
+export 'package:dpro_editor/src/ui/dpro_editor/dpro_editor.dart';
---
+++ lib/src/ui/dpro_editor/dpro_editor.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DproEditor extends StatefulWidget {
+  const DproEditor({super.key});
+
+  @override
+  State<DproEditor> createState() => _DproEditorState();
+}
+
+class _DproEditorState extends State<DproEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+
+  
+}
